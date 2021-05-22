a52a53f8a7210c3ff3a8d8f9411a371bcfb41d5e
Michał Witczak <michalwitczak19@gmail.com>
2021-05-22 22:13:57 +0200

    Add project files.

---
+++ Waga/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace Waga
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblWynik = new System.Windows.Forms.Label();
+            this.btnOblicz = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.rdoKobieta = new System.Windows.Forms.RadioButton();
+            this.rdoMezczyzna = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(44, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Podaj wzrost [cm]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(44, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Podaj płeć";
+            // 
+            // lblWynik
+            // 
+            this.lblWynik.AutoSize = true;
+            this.lblWynik.Location = new System.Drawing.Point(44, 251);
+            this.lblWynik.Name = "lblWynik";
+            this.lblWynik.Size = new System.Drawing.Size(40, 15);
+            this.lblWynik.TabIndex = 2;
+            this.lblWynik.Text = "Wynik";
+            this.lblWynik.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnOblicz
+            // 
+            this.btnOblicz.Location = new System.Drawing.Point(203, 162);
+            this.btnOblicz.Name = "btnOblicz";
+            this.btnOblicz.Size = new System.Drawing.Size(163, 44);
+            this.btnOblicz.TabIndex = 3;
+            this.btnOblicz.Text = "Oblicz";
+            this.btnOblicz.UseVisualStyleBackColor = true;
+            this.btnOblicz.Click += new System.EventHandler(this.btnOblicz_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(420, 45);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(151, 87);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Waga";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 48);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(64, 19);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Idealna";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 23);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 19);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Standardowa";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // rdoKobieta
+            // 
+            this.rdoKobieta.AutoSize = true;
+            this.rdoKobieta.Location = new System.Drawing.Point(217, 88);
+            this.rdoKobieta.Name = "rdoKobieta";
+            this.rdoKobieta.Size = new System.Drawing.Size(65, 19);
+            this.rdoKobieta.TabIndex = 5;
+            this.rdoKobieta.TabStop = true;
+            this.rdoKobieta.Text = "Kobieta";
+            this.rdoKobieta.UseVisualStyleBackColor = true;
+            // 
+            // rdoMezczyzna
+            // 
+            this.rdoMezczyzna.AutoSize = true;
+            this.rdoMezczyzna.Location = new System.Drawing.Point(300, 88);
+            this.rdoMezczyzna.Name = "rdoMezczyzna";
+            this.rdoMezczyzna.Size = new System.Drawing.Size(82, 19);
+            this.rdoMezczyzna.TabIndex = 6;
+            this.rdoMezczyzna.TabStop = true;
+            this.rdoMezczyzna.Text = "Mężczyzna";
+            this.rdoMezczyzna.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(217, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 23);
+            this.textBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 291);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rdoMezczyzna);
+            this.Controls.Add(this.rdoKobieta);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnOblicz);
+            this.Controls.Add(this.lblWynik);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Waga";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblWynik;
+        private System.Windows.Forms.Button btnOblicz;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoKobieta;
+        private System.Windows.Forms.RadioButton rdoMezczyzna;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Waga/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Waga
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnOblicz_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Podaj wzrost", "Błąd");
+                return;
+            }
+            if (!double.TryParse(textBox1.Text, out double wzrost))
+            {
+                MessageBox.Show("Nieprawidłowy wzrost", "Błąd");
+                return;
+            }
+            double standardowa = wzrost - 100;
+            var results = new List<string>();
+            if (checkBox1.Checked)
+            {
+                results.Add($"Twoja standardowa waga powinna wynosić: {standardowa} kg");
+            }
+            if (checkBox2.Checked)
+            {
+                if (!rdoKobieta.Checked && !rdoMezczyzna.Checked)
+                {
+                    MessageBox.Show("Podaj płeć", "Błąd");
+                    return;
+                }
+                var wsp = rdoKobieta.Checked ? 0.85 : 0.9;
+                double idealna = standardowa * wsp;
+                results.Add($"Twoja idealna waga powinna wynosić: {idealna} kg");
+            }
+            lblWynik.Text = string.Join('\n', results);
+        }
+    }
+}
